2cca5758a28f58e907a852dfc9854b031b7247a0
Hamza Ahmed Shaikh <99156466+HamzaAhmed04@users.noreply.github.com>
2025-08-27 16:58:07 -0700

    commit

---
+++ app.js
@@ -0,0 +1,112 @@
+// // Q 1
+
+
+// var firstName = prompt ("Enter First Name").toUpperCase()
+// var lastName = prompt ("Enter Last Name").toUpperCase()
+
+// alert("Welcome".toUpperCase() +" "+ firstName +" "+ lastName)
+
+// // Q 2
+
+
+// var device = prompt ("favorite mobile phone model")
+
+// document.writeln("My Favourite Device is: " +" "+ device +" "+ "<br>" + "Length of string" +" "+ device.length)
+
+// // Q 3
+
+
+// var word = ("Pakistani")
+
+// document.writeln ("String:" +" "+ word +" "+  "<br>" + "index of 'n':" +" "+ word.indexOf("n"))
+
+// // Q 4
+
+
+// var word = ("Hello World")
+
+// document.writeln ("String:" +" "+ word +" "+  "<br>" + "index of 'l':" +" "+ word.lastIndexOf("l"))
+
+// Q 5
+
+
+// var word = ("Pakistani")
+
+// document.writeln ("String:" +" "+ word +" "+  "<br>" + "Character at index 3:" +" "+ word.charAt(3))
+
+// // Q 6
+
+
+// var firstName = prompt ("Enter First Name")
+// var lastName = prompt ("Enter Last Name")
+
+// var merge = firstName.concat (" " ,lastName).toUpperCase()
+
+// alert("Welcome".toUpperCase() +" "+ merge)
+
+// // Q 7
+
+
+// var word = ("Hyderabad")
+
+// document.writeln ("City:" +" "+ "Hyderabad" + "<br>" +" "+ "After replacement:" +" "+ word.replace("Hyder" , "Islam"))
+
+// // Q 8
+
+
+// var message = ("Ali and Sami are best friends. They play cricket and football together.")
+
+// document.writeln (message.replaceAll("and" , "&"))
+
+// // Q 9
+
+// var num = "678"
+// var newNum = Number("678")
+
+// document.writeln("Value:" + " " + num  + "<br>")
+// document.writeln("Type:" + " " + typeof(num) + "<br>")
+// document.writeln("Value:" + " " + newNum  + "<br>")
+// document.writeln("Type:" + " " + typeof(newNum)  + "<br>")
+
+// // Q 10
+
+// let sweet = prompt("Enter your favourite sweet.")
+
+// document.writeln("Favourite Sweet:" + " " + sweet + "<br>")
+// document.writeln("Updated Favourite Sweet:" + " " + sweet.toUpperCase())
+
+// // Q 11
+
+// var lang = prompt("Your Fav Programming Lang?");
+
+// var res = lang.charAt(0).toUpperCase() + lang.slice(1);
+
+// document.writeln(res);
+
+// // Q 12
+
+// var num1 = 35.36
+// var num2 = num1.toString()
+// console.log(num2.replace(".", ""))
+
+// Q 13
+
+var username = prompt("Enter your username:");
+
+var valid = true;
+
+for (var i = 0; i < username.length; i++) {
+    var charCode = username.charCodeAt(i);
+
+
+    if (charCode === 33 || charCode === 44 || charCode === 46 || charCode === 64) {
+        valid = false;
+        break;
+    }
+}
+
+if (!valid) {
+    alert("Please enter a valid username without @ . , !");
+} else {
+    alert("Valid username: " + username);
+}
